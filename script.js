c800e6fcea4a23a3aa716628207dbabde8d63069
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-09-21 01:19:46 +0600

    updating all files

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let apiQuotes = [];
+
 // Get Quotes From Api
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        // eslint-disable-next-line no-undef
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+    } catch (err) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
